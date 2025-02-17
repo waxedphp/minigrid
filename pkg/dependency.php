8be5feccf3da1a128333b6b491a7d15ce577c597
Andy <andy@zoltan.zde>
2025-02-17 18:59:05 +0100

    Updated 2025-02-17

--- pkg/dependency.php
+++ pkg/dependency.php
@@ -3,9 +3,9 @@
 return [
   'js' => [
     NODE . '/minigrid/dist/minigrid.min.js',
-    '/minigrid/plugin.js',
+    $PATH . '/plugin.js',
   ],
   'css' => [
-    '/minigrid/style.css',
+    $PATH . '/style.css',
   ],
 ];
--- pkg/install.sh
+++ pkg/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-npm install --save minigrid
+npm install minigrid
